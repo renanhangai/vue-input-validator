a427abe6b80af297cf3e685b929ceffe4eb150cd
renanhangai <hangai.renan@gmail.com>
2017-03-15 14:07:02 -0300

    Added args for rules

--- src/Rules.js
+++ src/Rules.js
@@ -7,7 +7,9 @@ export default class Rules {
 	static register( name, rule ) {
 		$registered[ name ] = rule;
 	};
-
+	/**
+	 *
+	 */
 	static validate( value, rules, options ) {
 		const state = {
 			initial: value,
@@ -22,35 +24,47 @@ export default class Rules {
 			return result;
 		});
 	};
-	static _validate( value, rules, options, state ) {
+	/**
+	 *
+	 */
+	static _validate( value, rules, options, state, data ) {
 		if ( Array.isArray( rules ) ) {
-			return Rules._validateArray( value, rules, options, state, 0 );
+			return Rules._validateArray( value, rules, options, state, {}, 0 );
 		} else if ( typeof(rules) === 'string' ) {
-			rules = rules.split( "|" );
-			for ( let i = 0, len = rules.length; i<len; ++i )
-				rules[ i ] = rules[i].trim();
+			rules = trimMap( rules.split( "|" ) );
 			if ( rules.length > 1 )
-				return Rules._validateArray( value, rules, options, state, 0 );
+				return Rules._validateArray( value, rules, options, state, {}, 0 );
 
-			const newRule = $registered[ rules ];
+			const args = trimMap( rules.split(":") );
+			const newRule = $registered[ args[0] ];
 			if ( newRule == null )
 				throw new Error( `Invalid rule "${rules}"` );
-			return Rules._validate( value, newRule, options, state );
+			return Rules._validate( value, newRule, options, state, { args: args.slice(1) } );
 		} else if ( typeof(rules) === 'function' ) {
-			return rules.call( null, value, state );
+			const args = [value].concat(( data && data.args ) || []);
+			return rules.apply( null, args );
 		}
 		throw new Error( "Invalid rule" );
 	};
-	static _validateArray( value, rules, options, state, offset ) {
+	/**
+	 * Validate an array of objects
+	 */
+	static _validateArray( value, rules, options, state, data, offset ) {
 		offset = offset | 0;
-		return options.Promise.resolve( Rules._validate( value, rules[offset], options, state ) )
+		return options.Promise.resolve( Rules._validate( value, rules[offset], options, state, data ) )
 			.then(( result ) => {
 				if ( ( result === false ) || ( result == null ) )
 					throw new Error( "Invalid value" );
 				else if ( result === true )
 					result = value;
-				return Rules._validateArray( result, rules, options, state, offset+1 );
+				return Rules._validateArray( result, rules, options, state, data, offset+1 );
 			})
 		;
 	};
 };
+
+function trimMap( map ) {
+	for ( let i = 0, len = map.length; i<len; ++i )
+		map[ i ] = map[i].trim();
+	return map;
+}
